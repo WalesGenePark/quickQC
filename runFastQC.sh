34929001f52af4ba9d6fc7ccd45648d58f620d47
peterjgiles <79471080+peterjgiles@users.noreply.github.com>
2022-05-11 15:58:53 +0100

    Update runFastQC.sh

--- runFastQC.sh
+++ runFastQC.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-module load singularity
-
-
 #https://wotan.cardiff.ac.uk/containers/fastqc_v0.11.9.sif
 #fastqc --outdir $outqcdir/$lane $fq
+
+module load singularity
 
 singularity run --bind /data09:/data09 /data09/QC_pipelines/workflow/fastqc_v0.11.9.sif --outdir $outqcdir/$lane $fq
